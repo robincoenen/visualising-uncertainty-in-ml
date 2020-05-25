bf22add995b61cef9a1d39f6e336ce0237679621
EC2 Default User <ec2-user@ip-172-31-85-65.ec2.internal>
2020-05-25 17:47:42 +0000

    cleaned code

--- public/script.js
+++ public/script.js
@@ -1,7 +1,6 @@
 function myFunction(){
     const input = document.getElementById("myText").value;
     $.get( '/perspective',{text:input}, function(data) {       
-        console.log('get new results')
         let result = data*100
         console.log(result);
            if (result < 3){
@@ -27,36 +26,3 @@ function myFunction(){
         }
     });
 }
-
-
-// function myFunction(){
-//     const input = document.getElementById("myText").value;
-//     $.get( '/perspective',{text:input}, function(data) {       
-//        console.log('get new results')
-//        let result = data*100
-//        $('#demo').html(`<b>${result.toFixed(2)}</b>`)
-//        if (result < 3){
-//             $("#CircleA").attr('id', '#CircleB');
-//          } else if (result < 5) {
-//             $("#myImage").attr("src","3-5.gif"); 
-//          } else if (result < 7) {
-//             $("#myImage").attr("src","5-7.gif"); 
-//          } else if (result < 9) {
-//             $("#myImage").attr("src","7-9.gif"); 
-//          }  else if (result < 91) {
-//             $("#myImage").attr("src","sonst_1.gif"); 
-//          }else if (result < 93) {
-//             $("#myImage").attr("src","91-93.gif"); 
-//          } else if (result < 95) {
-//             $("#myImage").attr("src","93-95.gif"); 
-//          } else if (result < 97) {
-//             $("#myImage").attr("src","95-97.gif"); 
-//          } else if (result < 100) {
-//             $("#myImage").attr("src","97-100.gif"); 
-//          }  else {
-//             $("#myImage").attr("src","status.png"); 
-//        }
-//     });
-// }
-
-//.attr("src","second.jpg");
--- request.js
+++ request.js
@@ -23,24 +23,3 @@ app.get('/perspective', async function (req, res) {
  
 app.listen(8080, function () {  console.log('Example app listening on port 8080!');});
 app.use(express.static('public'));
-
-//const input = "Trump is a weird motherfucker"
-//async function main(input) {
-//    const perspectiveClient = new perspective(
-//        options = {
-//            "apiKey": API_KEY}
-//    )
-//    
-//    const text = input;
-//    const result = await perspectiveClient.analyze(text);
-//    const toxicScore = result.attributeScores.TOXICITY.summaryScore.value;
-//    console.log(`Probabiltiy of [${text}] is toxic is: ${toxicScore}`)
-//} 
-//
-//function myFunction() {
-//   const input = document.getElementById("myText").value;
-//  document.getElementById("demo").innerHTML = input;
-//  console.log(input)
-//  main(input);
-//
-//}
